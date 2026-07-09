1ee8b239ca6ed912241951342e85ea63d9dae99d
Axel Tan <68768816+axeltanxl@users.noreply.github.com>
2026-07-09 17:08:16 +0800

    feat: server and client code

--- client.cpp
+++ client.cpp
@@ -3,69 +3,100 @@
 
 #include <boost/asio.hpp>
 #include <iostream>
-using namespace std;
-using namespace boost::asio;
-using namespace boost::asio::ip;
+#include <string>
 
-class Client {
+namespace chat {
+
+// Owns the socket and handles the raw send/receive protocol.
+class ChatClient {
 public:
-    ip::tcp::socket client_socket;
+    explicit ChatClient(boost::asio::io_context& io_context)
+        : socket_(io_context) {}
 
-    Client(io_context& io_context) : client_socket(io_context) {}
-
-    void connect(const string& host, unsigned short port) {
-        client_socket.connect(tcp::endpoint(make_address(host), port));
+    void connect(const std::string& host, unsigned short port) {
+        socket_.connect(boost::asio::ip::tcp::endpoint(
+            boost::asio::ip::make_address(host), port));
     }
 
-    void sendData(const string& message) {
-        write(client_socket, buffer(message + "\n"));
+    void send(const std::string& message) {
+        boost::asio::write(socket_, boost::asio::buffer(message + "\n"));
     }
 
-    string getData() {
+    std::string receive() {
         boost::asio::streambuf buf;
-        read_until(client_socket, buf, "\n");
-        istream is(&buf);
-        string data;
-        getline(is, data);
+        boost::asio::read_until(socket_, buf, "\n");
+        std::istream is(&buf);
+        std::string data;
+        std::getline(is, data);
         return data;
     }
+
+    void close() {
+        if (socket_.is_open()) {
+            boost::system::error_code ec;
+            socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
+            socket_.close(ec);
+        }
+    }
+
+private:
+    boost::asio::ip::tcp::socket socket_;
 };
 
-int main(int argc, char* argv[])
-{
-    io_context io_context;
-    Client client(io_context);
-    client.connect("127.0.0.1", 9999);
+// Owns the chat loop / console interaction, independent of the transport.
+class ChatSession {
+public:
+    ChatSession(ChatClient& client, std::string username)
+        : client_(client), username_(std::move(username)) {}
 
-    // Getting username from user
-    cout << "Enter your name: ";
-    string u_name, reply, response;
-    getline(cin, u_name);
+    void run() {
+        client_.send(username_);
 
-    // Sending username to another end
-    // to initiate the conversation
-    client.sendData(u_name);
+        while (true) {
+            const std::string response = client_.receive();
+            if (response == "exit") {
+                std::cout << "Connection terminated" << std::endl;
+                break;
+            }
+            std::cout << "Server: " << response << std::endl;
 
-    // Infinite loop for chit-chat
-    while (true) {
-
-        // Fetching response
-        response = client.getData();
-
-        // Validating if the connection has to be closed
-        if (response == "exit") {
-            cout << "Connection terminated" << endl;
-            break;
+            std::cout << username_ << ": ";
+            std::string reply;
+            if (!std::getline(std::cin, reply) || reply == "exit") {
+                client_.send("exit");
+                break;
+            }
+            client_.send(reply);
         }
-        cout << "Server: " << response << endl;
-
-        // Reading new message from input stream
-        cout << u_name << ": ";
-        getline(cin, reply);
-        client.sendData(reply);
-
-        if (reply == "exit")
-            break;
     }
+
+private:
+    ChatClient& client_;
+    std::string username_;
+};
+
+} // namespace chat
+
+int main(int argc, char* argv[]) {
+    const std::string host = argc > 1 ? argv[1] : "127.0.0.1";
+    const unsigned short port =
+        static_cast<unsigned short>(argc > 2 ? std::stoi(argv[2]) : 9999);
+
+    try {
+        boost::asio::io_context io_context;
+        chat::ChatClient client(io_context);
+        client.connect(host, port);
+
+        std::cout << "Enter your name: ";
+        std::string username;
+        std::getline(std::cin, username);
+
+        chat::ChatSession session(client, username);
+        session.run();
+    } catch (const std::exception& e) {
+        std::cerr << "Failed to connect/communicate: " << e.what() << std::endl;
+        return 1;
+    }
+
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -4,73 +4,93 @@
 #include <boost/asio.hpp>
 #include <iostream>
 
-using namespace std;
-using namespace boost::asio;
-using namespace boost::asio::ip;
+namespace chat {
 
-// Driver program for receiving data from buffer
-string getData(tcp::socket& socket)
-{
-    boost::asio::streambuf buf;
-    read_until(socket, buf, "\n");
-    istream is(&buf);
-    string data;
-    getline(is, data);
-    return data;
-}
+class ChatServer {
 
-// Driver program to send data
-void sendData(tcp::socket& socket, const string& message)
-{
-    write(socket,
-          buffer(message + "\n"));
-}
+  public:
+    ChatServer(boost::asio::io_context& io_context, unsigned short port)
+        : acceptor_server_(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)),
+          server_socket_(io_context) {}
+
+    // Driver program for receiving data from buffer
+    std::string getData()
+    {
+        boost::asio::streambuf buf;
+        boost::asio::read_until(server_socket_, buf, "\n");
+        std::istream is(&buf);
+        std::string data;
+        std::getline(is, data);
+        return data;
+    }
+
+    // Driver program to send data
+    void sendData(const std::string& message)
+    {
+        boost::asio::write(server_socket_, boost::asio::buffer(message + "\n"));
+    }
+
+    void acceptConnection() {
+        acceptor_server_.accept(server_socket_);
+    }
+
+
+  private:
+    boost::asio::ip::tcp::acceptor acceptor_server_;
+    boost::asio::ip::tcp::socket server_socket_;
+};
+
+class ChatSession {
+  public:
+    ChatSession(ChatServer& server, std::string username)
+        : server_(server), username_(std::move(username)) {}
+
+    void run() {
+        // Replying with default message to initiate chat
+        std::string response, reply;
+        reply = "Hello " + username_ + "!";
+        std::cout << "Server: " << reply << std::endl;
+        server_.sendData(reply);
+
+        while (true) {
+            const std::string response = server_.getData();
+            if (response == "exit") {
+                std::cout << username_ << " left!" << std::endl;
+                break;
+            }
+            std::cout << username_ << ": " << response << std::endl;
+
+            // Reading new message from input stream
+            std::cout << "Server"
+                      << ": ";
+            std::getline(std::cin, reply_);
+            server_.sendData(reply_);
+
+            if (reply_ == "exit")
+                break;
+        }
+    }
+
+  private:
+    ChatServer& server_;
+    std::string username_;
+    std::string reply_;
+};
+
+} // namespace chat
 
 int main(int argc, char* argv[])
 {
-    io_context io_context;
+    boost::asio::io_context io_context;
 
-    // Listening for any new incomming connection
-    // at port 9999 with IPv4 protocol
-    tcp::acceptor acceptor_server(
-        io_context,
-        tcp::endpoint(tcp::v4(), 9999));
-
-    // Creating socket object
-    tcp::socket server_socket(io_context);
+    chat::ChatServer chat_server(io_context, 9999);
 
     // waiting for connection
-    acceptor_server.accept(server_socket);
+    chat_server.acceptConnection();
 
     // Reading username
-    string u_name = getData(server_socket);
+    std::string u_name = chat_server.getData();
 
-    // Replying with default message to initiate chat
-    string response, reply;
-    reply = "Hello " + u_name + "!";
-    cout << "Server: " << reply << endl;
-    sendData(server_socket, reply);
-
-    while (true) {
-
-        // Fetching response
-        response = getData(server_socket);
-
-        // Validating if the connection has to be closed
-        if (response == "exit") {
-            cout << u_name << " left!" << endl;
-            break;
-        }
-        cout << u_name << ": " << response << endl;
-
-        // Reading new message from input stream
-        cout << "Server"
-             << ": ";
-        getline(cin, reply);
-        sendData(server_socket, reply);
-
-        if (reply == "exit")
-            break;
-    }
-    return 0;
+    chat::ChatSession session(chat_server, u_name);
+    session.run();
 }
