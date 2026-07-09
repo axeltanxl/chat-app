c8241e27e0b5968d4557f426fd1491acd7e635e3
Axel Tan <68768816+axeltanxl@users.noreply.github.com>
2026-07-09 18:48:26 +0800

    feat: add ChatManager

--- server.cpp
+++ server.cpp
@@ -73,6 +73,34 @@ private:
     std::string reply_;
 };
 
+class ChatManager {
+public:
+    void add(const std::string &username, std::shared_ptr<ChatSession> session){
+        std::lock_guard<std::mutex> lock(mutex_);
+        clients_[username] = session;
+    };
+    void remove(const std::string &username){
+        std::lock_guard<std::mutex> lock(mutex_);
+        clients_.erase(username);
+    };
+    void sendTo(const std::string &username, const std::string &message){
+        std::lock_guard<std::mutex> lock(mutex_);
+        auto it = clients_.find(username);
+        if (it != clients_.end()) {
+            it->second->sendData(username, message);
+        }
+    }
+    void broadcast(const std::string &message){
+        std::lock_guard<std::mutex> lock(mutex_);
+        for (const auto &[username, session] : clients_) {
+            session->sendData(username, message);
+        }
+    }
+private:
+    std::mutex mutex_;
+    std::unordered_map<std::string, std::shared_ptr<ChatSession>> clients_;
+};
+
 } // namespace chat
 
 int main(int argc, char *argv[]) {
