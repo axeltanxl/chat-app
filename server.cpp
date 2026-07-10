6d478a0a5dce53822ee96be981deae2b4c5813f8
Axel Tan <68768816+axeltanxl@users.noreply.github.com>
2026-07-10 18:13:43 +0800

    feat: allow multiple clients to broadcast messages to each other

--- server.cpp
+++ server.cpp
@@ -3,103 +3,108 @@
 
 #include <boost/asio.hpp>
 #include <iostream>
+#include <thread>
 
 namespace chat {
+
+// Driver program for receiving data from buffer
+std::string readLine(boost::asio::ip::tcp::socket &socket) {
+    boost::asio::streambuf buf;
+    boost::asio::read_until(socket, buf, "\n"); // reads until newline character is found
+    std::istream is(&buf);
+    std::string data;
+    std::getline(is, data);
+    return data;
+}
 
 class ChatServer {
 public:
     ChatServer(boost::asio::io_context &io_context, unsigned short port)
-        : acceptor_server_(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)),
-          server_socket_(io_context) {}
+        : io_context_(io_context), acceptor_(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)) {}
+
+    std::shared_ptr<boost::asio::ip::tcp::socket> acceptConnection() {
+        auto socket = std::make_shared<boost::asio::ip::tcp::socket>(io_context_);
+        acceptor_.accept(*socket);
+        return socket;
+    }
+
+private:
+    boost::asio::io_context& io_context_;
+    boost::asio::ip::tcp::acceptor acceptor_;
+};
+
+class ChatManager;   // forward declaration: ChatSession only needs a reference to it below
+
+class ChatSession {
+public:
+    ChatSession(std::shared_ptr<boost::asio::ip::tcp::socket> socket, std::string username, ChatManager& manager)
+        : socket_(std::move(socket)), username_(std::move(username)), manager_(manager) {}
 
     // Driver program for receiving data from buffer
     std::string getData() {
-        boost::asio::streambuf buf;
-        boost::asio::read_until(server_socket_, buf, "\n"); // reads until newline character is found
-        std::istream is(&buf);
-        std::string data;
-        std::getline(is, data);
-        return data;
+        return readLine(*socket_);
     }
 
     // Driver program to send data
     void sendData(const std::string &message) {
-        boost::asio::write(server_socket_, boost::asio::buffer(message + "\n"));
+        boost::asio::write(*socket_, boost::asio::buffer(message + "\n"));
     }
 
-    void acceptConnection() {
-        acceptor_server_.accept(server_socket_);
-    }
+    void run();   // defined below, once ChatManager is fully declared
 
 private:
-    boost::asio::ip::tcp::acceptor acceptor_server_;
-    boost::asio::ip::tcp::socket server_socket_;
-};
-
-class ChatSession {
-public:
-    ChatSession(ChatServer &server, std::string username)
-        : server_(server), username_(std::move(username)) {}
-
-    void run() {
-        // Replying with default message to initiate chat
-        std::string response, reply;
-        reply = "Hello " + username_ + "!";
-        std::cout << "Server: " << reply << std::endl;
-        server_.sendData(reply);
-
-        while (true) {
-            const std::string response = server_.getData();
-            if (response == "exit") {
-                std::cout << username_ << " left!" << std::endl;
-                break;
-            }
-            std::cout << username_ << ": " << response << std::endl;
-
-            // Reading new message from input stream
-            std::cout << "Server"
-                      << ": ";
-            std::getline(std::cin, reply_);
-            server_.sendData(reply_);
-
-            if (reply_ == "exit")
-                break;
-        }
-    }
-
-private:
-    ChatServer &server_;
+    std::shared_ptr<boost::asio::ip::tcp::socket> socket_;
     std::string username_;
     std::string reply_;
+    ChatManager& manager_;
 };
 
 class ChatManager {
 public:
-    void add(const std::string &username, std::shared_ptr<ChatSession> session){
+    void add(const std::string &username, std::shared_ptr<ChatSession> session) {
         std::lock_guard<std::mutex> lock(mutex_);
         clients_[username] = session;
     };
-    void remove(const std::string &username){
+
+    void remove(const std::string &username) {
         std::lock_guard<std::mutex> lock(mutex_);
         clients_.erase(username);
     };
-    void sendTo(const std::string &username, const std::string &message){
+
+    void sendTo(const std::string &username, const std::string &message) {
         std::lock_guard<std::mutex> lock(mutex_);
         auto it = clients_.find(username);
         if (it != clients_.end()) {
-            it->second->sendData(username, message);
+            it->second->sendData(message);
         }
-    }
-    void broadcast(const std::string &message){
+    };
+
+    void broadcast(const std::string &message) {
         std::lock_guard<std::mutex> lock(mutex_);
         for (const auto &[username, session] : clients_) {
-            session->sendData(username, message);
+            session->sendData(message);
         }
     }
+
 private:
     std::mutex mutex_;
     std::unordered_map<std::string, std::shared_ptr<ChatSession>> clients_;
 };
+
+// ChatManager is complete now, so ChatSession::run() can call manager_.broadcast(...)
+void ChatSession::run() {
+    sendData("Hello " + username_ + "!");
+
+    while (true) {
+        const std::string message = getData();
+        if (message == "exit") {
+            std::cout << username_ << " left!" << std::endl;
+            break;
+        }
+        std::cout << username_ << ": " << message << std::endl;
+        manager_.broadcast(username_ + ": " + message);
+    }
+}
 
 } // namespace chat
 
@@ -108,12 +113,22 @@ int main(int argc, char *argv[]) {
 
     chat::ChatServer chat_server(io_context, 9999);
 
-    // waiting for connection
-    chat_server.acceptConnection();
+    chat::ChatManager chat_manager;
 
-    // Reading username
-    std::string u_name = chat_server.getData();
+    while(true){
+        // waiting for connection
+        auto socket = chat_server.acceptConnection();
 
-    chat::ChatSession session(chat_server, u_name);
-    session.run();
+        // get username from client
+        std::string u_name = chat::readLine(*socket);
+
+        auto session = std::make_shared<chat::ChatSession>(socket, u_name, chat_manager);
+
+        chat_manager.add(u_name, session);
+
+        std::thread([session, &chat_manager, u_name]() {
+            session->run();
+            chat_manager.remove(u_name);
+        }).detach();
+    }
 }
